1e0fb71127a3588fb95e2be36ed13035ebcf506c
Shalbandians <112478807+Shalbandians@users.noreply.github.com>
2024-03-13 09:30:19 +0500

    done

--- models/userSchema.js
+++ models/userSchema.js
@@ -23,6 +23,9 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
         minlength: 8
+    }, verified: {
+        type: Boolean,
+        default: false,
     },
     token: [
         {
